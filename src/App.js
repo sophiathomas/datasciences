ea4c1a3e3b1301a6f7b36efde5201394330ee833
Sophia Thomas <sophia.karah.thomas@gmail.com>
2020-08-30 22:01:19 -0400

    Reafactored to proper error handling using React re-rendering of updating states

--- src/App.js
+++ src/App.js
@@ -10,18 +10,32 @@ class App extends Component {
     this.handleTextChange = this.handleTextChange.bind(this);
     this.handleCheckboxChange = this.handleCheckboxChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
-    this.getInitialState = this.getInitialState.bind(this);
+    this.resetForm = this.resetForm.bind(this);
+    this.validateForm = this.validateForm.bind(this);
   }
 
   getInitialState = () => {
     return {
-      inputs: {
+      data: {
         name: '',
         email: '',
         postalCode: '',
         consent: false,
-      }
+      },
+      inputs: {
+        consent: {
+          value: "I agree",
+        },
+        submit: {
+          value: "Submit",
+        }
+      },
+      errors: false
     };
+  }
+
+  resetForm = () => {
+    this.setState(this.getInitialState());
   }
   
   handleTextChange = event => {
@@ -29,8 +43,8 @@ class App extends Component {
     const value = event.target.value;
 
     this.setState({ 
-      inputs: {
-        ...this.state.inputs, [name]: value 
+      data: {
+        ...this.state.data, [name]: value 
       } 
     });
   }
@@ -40,64 +54,70 @@ class App extends Component {
     const checked = event.target.checked ? true : false;
     
     this.setState({
-      inputs: {
-        ...this.state.inputs, [name]: checked
+      data: {
+        ...this.state.data, [name]: checked
       }
     });
   }
 
-  handleSubmit = event => {
-    event.preventDefault(); 
-    
-    const inputs = event.target.elements;
-    const info = this.state.inputs;
-    const errors = getErrors(info);
-
-    //reset errors
-    Object.entries(inputs).forEach( ([key, input]) => {
-      input.classList.remove('error');
-    });
-
-    if(errors) {
-      // display error to client
-      Object.entries(errors).forEach( ([inputName, message]) => {
-        inputs[inputName].classList.add('error');
+  validateForm = data => {
+    try {
+      return new Promise((resolve, reject) => {
+        // Updating the state displays the errors on the form
+        this.setState({
+          errors: (getErrors(data) || false),
+        },() => resolve(!this.state.errors));
       });
-      return; 
-    } else { 
-      
-      // async fetch to disable double submition
-      fetch('https://webhook.site/bdf2b2f6-c540-4b0b-b424-2a99f5e3b66f', {
-        method: 'POST',
-        mode: 'no-cors',
-        body: info,
-      })
-      .then(data => {
-        console.log('Success:', data);
-
-        // confirm to client
-        document.getElementById('signup').value = "Thanks for signing up";
-        
-      })
-      .catch(error => {
-        console.error('Error:', error);
-
-        // something went wrong, please refresh the page
-        document.getElementById('signup').value = "A problem occurred, please try again";
-      })
-      .finally( () => {
-        setTimeout(this.resetForm, 2000);
-      });
+    } catch(e) {
+      // @TODO: Do something better here
+      console.log(e);
     }
   }
 
-  displayErrors = errors => {
-    // todo ^^
-  }
+  handleSubmit = async event => {
+    event.preventDefault();
 
-  resetForm = () => {
-    document.getElementById('signup').value = "Submit";
-    this.setState(this.getInitialState());
+    let isValid = await this.validateForm(this.state.data);
+
+    if(isValid) {
+      const info = this.state.data;
+      alert(JSON.stringify(info));
+
+      // async fetch to disable double submition
+      fetch("https://webhook.site/bdf2b2f6-c540-4b0b-b424-2a99f5e3b66f", {
+        method: "POST",
+        mode: "no-cors",
+        body: info,
+      })
+      .then(function(response) {
+        return response.text();
+      })
+      .then(data => {
+        console.log("Success:", data);
+
+        // confirm to client
+        // @TODO: Update the div form to display a nice confirmation
+        this.setState({
+          inputs: {
+            ...this.state.inputs, "submit": { value: "Thanks for signing up"}
+          }
+        });
+        
+      })
+      .catch(error => {
+        console.error("Error:", error);
+
+        // something went wrong
+        this.setState({
+          inputs: {
+            ...this.state.inputs, "submit": { value: "A problem occurred, please try again"}
+          }
+        });
+      })
+      .finally( () => {
+        setTimeout(this.resetForm, 3000);
+      });
+    }
   }
 
   render() {
@@ -105,21 +125,38 @@ class App extends Component {
       <form onSubmit={this.handleSubmit}>
         <label>
           Name:
-          <input type="text" name="name" value={this.state.inputs.name} onChange={this.handleTextChange} required />
+          <input className={this.state.errors.name ? 'error': null} type="text" name="name" value={this.state.data.name} onChange={this.handleTextChange} required />
         </label>
+        {this.state.errors.name && 
+          this.state.errors.name.map((message, index, array) => {
+            return <span key={index}>{message}</span>;
+          })
+        }
+       
         <label>
           Email:
-          <input type="email" name="email" value={this.state.inputs.email} onChange={this.handleTextChange} required />
+          <input className={this.state.errors.email ? 'error': null} type="email" name="email" value={this.state.data.email} onChange={this.handleTextChange} required />
         </label>
+        {this.state.errors.email && 
+          this.state.errors.email.map((message, index, array) => {
+            return <span key={index}>{message}</span>;
+          })
+        }
+
         <label>
           Postal Code:
-          <input type="text" name="postalCode" value={this.state.inputs.postal} onChange={this.handleTextChange} required />
+          <input className={this.state.errors.postalCode ? 'error': null} type="text" name="postalCode" value={this.state.data.postalCode} onChange={this.handleTextChange} required />
         </label>
+        {this.state.errors.postalCode && 
+          this.state.errors.postalCode.map((message, index, array) => {
+            return <span key={index}>{message}</span>;
+          })
+        }
+
         <label>
-          <input type="checkbox" name="consent" value={this.state.inputs.consent} onChange={this.handleCheckboxChange} required />
-           I have read and agreed to the terms and conditions.
+          <input className={this.state.errors.consent ? 'error': null} type="checkbox" name="consent" value={this.state.inputs.consent.value} checked={this.state.data.consent} onChange={this.handleCheckboxChange} required /> I have read and agreed to the terms and conditions.
         </label>
-        <input type="submit" id="signup" value="Submit" />
+        <input type="submit" id="signup" value={this.state.inputs.submit.value} />
       </form>
     );
   }
@@ -132,7 +169,7 @@ const constraints = {
   email: {
     presence: {allowEmpty: false},
     email: {
-      message: "Please enter a valid email"
+      message: "^Please enter a valid email"
     }
   },
   postalCode: {
@@ -140,14 +177,14 @@ const constraints = {
     format: {
       pattern: /^(?:[A-Z]\d[A-Z][ -]?\d[A-Z]\d)$/i,
       flags: "i",
-      message: "Please enter a valid Canadian postal code"
+      message: "^Please enter a valid Canadian postal code"
     }
   },
   consent: {
     presence: true,
     inclusion: {
       within: [true],
-      message: "Please check the box to accept the terms and conditions"
+      message: "^Please check the box to accept the terms and conditions"
     }
   }
 }
